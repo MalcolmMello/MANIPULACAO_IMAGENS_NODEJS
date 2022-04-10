96da4da82ef7e92371ecf4c3d3d246e1fa54504b
MalcolmMello <malcolmmellolima@gmail.com>
2022-04-10 12:09:47 -0300

    finalizando minha primeira api

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { Sequelize } from 'sequelize';
 import { Phrase } from '../models/Phrase';
 
 export const ping = (req: Request, res: Response) => {
@@ -22,4 +23,50 @@ export const createPhrase = async (req: Request, res: Response) => {
 
     res.status(201);
     res.json({ id: newPhrase.id, author, txt })
-}
+};
+
+export const listPhrases = async (req: Request, res: Response) => {
+    let list = await Phrase.findAll();
+    res.json({ list });
+};
+
+export const getPhrase = async (req: Request, res: Response) => {
+    let { id } = req.params;
+    let phrase = await Phrase.findByPk(id);
+    if(phrase) res.json({ phrase });
+    else res.json({ error: 'Frase não encontrada' });
+};
+
+export const updatePhrase = async (req: Request, res: Response) => {
+    let { id } = req.params;
+    let { author, txt } = req.body;
+
+    let phrase = await Phrase.findByPk(id);
+
+    if(phrase) {
+        phrase.author = author;
+        phrase.txt = txt;
+        await phrase.save();
+
+        res.json({ phrase });
+    } else {
+        res.json({ error: 'Frase não encontrada' })
+    }
+};
+
+export const deletePhrase = async (req: Request, res: Response) => {
+    let { id } = req.params;
+    await Phrase.destroy({ where: { id }});
+    res.json({});
+};
+
+export const randomPhrase = async (req: Request, res: Response) => {
+    let phrase = await Phrase.findOne({
+        order: [
+            Sequelize.fn('RANDOM')
+        ]
+    })
+
+    if(phrase) res.json({ phrase });
+    else res.json({ error: 'Não há frases cadastradas' })
+};
