1943d46d047d1466fce23bc2d46b0523aa0f26ce
MalcolmMello <malcolmmellolima@gmail.com>
2022-04-14 10:54:30 -0300

    deletando imagem temporaria

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { unlink } from 'fs/promises';
 import { Sequelize } from 'sequelize';
 import sharp from 'sharp';
 import { Phrase } from '../models/Phrase';
@@ -61,12 +62,18 @@ export const randomPhrase = async (req: Request, res: Response) => {
 
 export const uploadFile = async (req: Request, res: Response) => {
     if(req.file) {
+        const filename = `${req.file.filename}.jpg`
         await sharp(req.file.path)
-        .resize(500)
-        .toFormat('jpeg')
-        .toFile(`./public/media/${req.file.filename}.jpg`)
+		    .resize(500, 500, {/* tipos de redimensionamento */
+					fit: sharp.fit.fill, // proporcional a img
+					position: 'bottom'
+				})
+		    .toFormat('jpeg')
+	        .toFile(`./public/media/${req.file.filename}.jpg`);
         
-        res.json({ image: `${req.file.filename}.jpg` });
+		await unlink(req.file.path);
+
+        res.json({ image: `${filename}` });
     } else {
         res.status(400);
         res.json({ error: 'Envie um arquivo válido' })
